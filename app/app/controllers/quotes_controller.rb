2d5b737bfb31b950a94ef9d21824bb74347d214d
Unusualslim <lance@perrybrothersoil.com>
2016-03-23 00:27:46 +0000

    Added the Quotes controller, through new method

--- app/app/controllers/quotes_controller.rb
+++ app/app/controllers/quotes_controller.rb
@@ -1,5 +1,20 @@
 class QuotesController < ApplicationController
+	def show
+		@quote = Quotes.find(params[:id])
+	end
+
 	def new
 	end
 
+	def create
+		@quote = Quote.new(quote_params)
+	
+		@quote.save
+		redirect_to @quote
+	end
+
+	private
+		def quote_params
+			params.require(:quote).permit(:quote, :author, :date)
+		end
 end
---
+++ app/db/migrate/20160323000057_create_quotes.rb
@@ -0,0 +1,11 @@
+class CreateQuotes < ActiveRecord::Migration
+  def change
+    create_table :quotes do |t|
+      t.string :quote
+      t.string :author
+      t.date :date
+
+      t.timestamps null: false
+    end
+  end
+end
