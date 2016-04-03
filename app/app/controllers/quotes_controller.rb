e6cf771db17ffa9ca171af0de2ab4a731f84f0bc
unusualslim <lance@perrybrothersoil.com>
2016-04-03 10:38:31 -0400

    QuoteController1.0

--- app/app/controllers/quotes_controller.rb
+++ app/app/controllers/quotes_controller.rb
@@ -25,6 +25,23 @@ class QuotesController < ApplicationController
 		end
 	end
 	
+	def update
+		@quote = Quote.find(params[:id])
+		
+		if @quote.update(quote_params)
+			redirect_to @quote
+		else
+			ender 'edit'
+		end
+	end
+	
+	def destroy
+		@quote = Quote.find(params [:id])
+		@quote.destroy
+		
+		redirect_to quotes_path
+	end
+	
 	private
 		def quote_params
 			params.require(:quotes).permit(:content, :author, :date)
