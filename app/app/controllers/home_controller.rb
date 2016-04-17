412057afc544664a9f2894e394c655cf3d23c767
unusualslim <lance@perrybrothersoil.com>
2016-04-17 10:12:14 -0400

    small mods

--- app/app/controllers/home_controller.rb
+++ app/app/controllers/home_controller.rb
@@ -1,11 +1,9 @@
 class HomeController < ApplicationController
-	$time = Time.new
-  	$time = time.strftime("%Y-%m-%d")
-
-	
 	def index
-	@quote = Quote.new
-	@quote = Quote.find_by day: $time
+	time = Time.new
+  	time = time.strftime("%Y-%m-%d")
+  	
+	@quote = Quote.find_by day: time
   end
   
 end
