8545669c6d899d9b124c29bec20f84e1dc357438
Unusualslim <lance@perrybrothersoil.com>
2016-06-14 00:10:43 +0000

    actionmailer

---
+++ app/app/mailers/usermailer.rb
@@ -0,0 +1,10 @@
+class Usermailer < ApplicationMailer
+	default from: 'notifications@onegearonedream.com'
+
+	def welcome_email(user)
+		@user = user
+		@url = 'http://onegearonedream.com/login'
+		mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+	end
+
+end
